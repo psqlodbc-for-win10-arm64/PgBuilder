8be415815f95c78298566cfc656c689490f5f52a
kenjiuno <ku@digitaldolphins.jp>
2025-11-13 19:03:22 +0900

    0.2

--- PgBuilder/Program.cs
+++ PgBuilder/Program.cs
@@ -39,38 +39,158 @@ namespace PgBuilder
 
             [Option("vc-lib", Required = true, HelpText = "Path to the VC lib.exe executable.")]
             public string VC_LIB { get; set; } = "";
+
+            [Option("silent", HelpText = "Suppress output messages during the build process.")]
+            public bool Silent { get; set; }
+        }
+
+        [Verb("postgresql-arm64x-prepare", HelpText = "Prepare PostgreSQL for building.")]
+        private class PostgreSQLPrepareOpt
+        {
+            [Value(0, Required = true, HelpText = "Path to the PostgreSQL source directory.")]
+            public string SourceDir { get; set; } = "";
+
+            [Option("meson", Required = true, HelpText = "Path to the meson.exe executable.")]
+            public string Meson { get; set; } = "";
+
+            [Option("prefix", Required = true, HelpText = "Path to the destination directory for the build output.")]
+            public string Prefix { get; set; } = "";
+
+            [Option("build-dir", Required = true, HelpText = "Path to the build directory where the build files will be generated.")]
+            public string BuildDir { get; set; } = "";
+
+            [Option("pkg-config", Required = true, HelpText = "Path to the pkg-config.exe executable.")]
+            public string PkgConfig { get; set; } = "";
+
+            [Option("pkg-config-dir", Required = true, HelpText = "Path to the directory where pkg-config files will be installed.")]
+            public string PkgConfigDir { get; set; } = "";
+
+            [Option("alt-cc", Required = true, HelpText = "Path to the Arm64XDualObjCL executable.")]
+            public string CC { get; set; } = "";
+
+            [Option("alt-ld", Required = true, HelpText = "Path to the Arm64XDualObjLINK executable.")]
+            public string LD { get; set; } = "";
+
+            [Option("alt-ar", Required = true, HelpText = "Path to the Arm64XDualObjLIB executable.")]
+            public string AR { get; set; } = "";
         }
 
         static int Main(string[] args)
         {
-            return Parser.Default.ParseArguments<OpenSSLPrepareOpt, OpenSSLBuildOpt>(args)
-                .MapResult<OpenSSLPrepareOpt, OpenSSLBuildOpt, int>(
+            return Parser.Default.ParseArguments<OpenSSLPrepareOpt, OpenSSLBuildOpt, PostgreSQLPrepareOpt>(args)
+                .MapResult<OpenSSLPrepareOpt, OpenSSLBuildOpt, PostgreSQLPrepareOpt, int>(
                     DoOpenSSLPrepare,
                     DoOpenSSLBuild,
+                    DoPostgreSQLPrepare,
                     ex => 1
                 );
         }
 
-        private static int DoOpenSSLBuild(OpenSSLBuildOpt o)
+        private static int DoPostgreSQLPrepare(PostgreSQLPrepareOpt o)
         {
+            {
+                var writer = new StringWriter() { NewLine = "\n", };
+                writer.WriteLine("[binaries]");
+                writer.WriteLine("c = '{0}'", o.CC);
+                writer.WriteLine("cpp = '{0}'", o.CC);
+                writer.WriteLine("ar = '{0}'", o.AR);
+                writer.WriteLine("c_ld = '{0}'", o.LD);
+                writer.WriteLine("cpp_ld = '{0}'", o.LD);
+                writer.WriteLine("link = '{0}'", o.LD);
+                writer.WriteLine("lld-link = '{0}'", o.LD);
+                writer.WriteLine("pkg-config = '{0}'", o.PkgConfig);
+                writer.WriteLine();
+                writer.WriteLine("[host_machine]");
+                writer.WriteLine("system = 'windows'");
+                writer.WriteLine("cpu_family = 'aarch64'");
+                writer.WriteLine("cpu = 'arm64'");
+                writer.WriteLine("endian = 'little'");
+
+                File.WriteAllText(
+                    Path.Combine(o.SourceDir, "arm64x.txt"),
+                    writer.ToString(),
+                    new UTF8Encoding(false)
+                );
+            }
+
+#if true
             var psi = new ProcessStartInfo(
-                "nmake.exe",
-                $" DESTDIR=\"{o.DestDir}\" install "
+                o.Meson,
+                string.Join(
+                    " ",
+                    "setup",
+                    "--cross-file arm64x.txt",
+                    "--prefix \"" + o.Prefix + "\"",
+                    "--backend ninja",
+                    "--buildtype release",
+                    "--pkg-config-path \"" + o.PkgConfigDir + "\"",
+                    "-Dplpython=disabled",
+                    "-Dldap=disabled",
+                    "-Dssl=openssl",
+                    "-Dplperl=disabled",
+                    "-Dreadline=disabled",
+                    "\"" + o.BuildDir + "\""
+                )
             )
             {
                 UseShellExecute = false,
                 WorkingDirectory = o.SourceDir,
             };
-            psi.Environment["CL_EXE"] = o.VC_CL;
-            psi.Environment["LINK_EXE"] = o.VC_LINK;
-            psi.Environment["LIB_EXE"] = o.VC_LIB;
             var p = Process.Start(psi)!;
             p.WaitForExit();
-
             if (p.ExitCode != 0)
             {
-                Console.Error.WriteLine("Failed to build OpenSSL.");
+                Console.Error.WriteLine("Failed to configure PostgreSQL.");
                 return p.ExitCode;
+            }
+#endif
+
+
+            return 0;
+        }
+
+        private static int DoOpenSSLBuild(OpenSSLBuildOpt o)
+        {
+            var nmakeOpts = (o.Silent) ? " /S" : "";
+
+            int NMake(string args)
+            {
+                var psi = new ProcessStartInfo(
+                    "nmake.exe",
+                    $"{nmakeOpts} {args}"
+                )
+                {
+                    UseShellExecute = false,
+                    WorkingDirectory = o.SourceDir,
+                };
+                psi.Environment["CL_EXE"] = o.VC_CL;
+                psi.Environment["LINK_EXE"] = o.VC_LINK;
+                psi.Environment["LIB_EXE"] = o.VC_LIB;
+                var p = Process.Start(psi)!;
+                p.WaitForExit();
+
+                if (p.ExitCode != 0)
+                {
+                    Console.Error.WriteLine("Failed to build OpenSSL.");
+                    return p.ExitCode;
+                }
+
+                return 0;
+            }
+
+            {
+                var errorlevel = NMake($" DESTDIR=\"{o.DestDir}\" ");
+                if (errorlevel != 0)
+                {
+                    return errorlevel;
+                }
+            }
+            {
+                var errorlevel = NMake($" DESTDIR=\"{o.DestDir}\" install ");
+                if (errorlevel != 0)
+                {
+                    return errorlevel;
+                }
             }
 
             return 0;
